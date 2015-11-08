2c6ef4ab508e18045dfcc5b7bc5758434b467840
Anastassiya Zidkova <anastassiya.zidkova@merck.com>
2015-11-08 23:03:29 +0100

    2 questions about string printing added

--- 01.py
+++ 01.py
@@ -1,10 +1,12 @@
 # This is just comment for you
 # Next line will show Hello world! message
-print "Hello world!"
+print("Hello world!")
 
 # Exercise
 # 1. Change text inside ""
 # 2. Try to use ' instead of "
 # 3. Try to print more lines of text
 # 4. Try to put " inside ""
-# 5. Try print 1+1
+# 5. Try print (1+1)
+# 6. Try print ("My name is " + "Stepan")?
+# 7. Try print ("Hello" * 5)  
