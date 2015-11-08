0d0572bb69a1b0dba6974409c5409e3e96f7c5a0
Anastassiya Zidkova <anastassiya.zidkova@merck.com>
2015-11-08 23:05:21 +0100

    Function hi() added

--- 07.py
+++ 07.py
@@ -1,3 +1,9 @@
+def hi():
+    print ("Hi there!")
+
+hi()
+hi("Stepan")
+
 def printColor(c):
 	# return statement returns value
 	return "{0}:x:{1:x}".format(c[0], c[1])
@@ -7,7 +13,7 @@ colors = [["pink", 0xFF1493], ["blue", 0x0000FF], ["red", 0xFF0000], ["green", 0
 	
 for color in colors:
 	# printColor returns string value
-	print printColor(color)
+	print (printColor(color))
 	
 # Exercise
 # 1. Try to move function to the end of code
